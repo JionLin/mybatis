7d6e9562788a365615f00b3febf6c2c43139bbcd
JionLin <250912986@qq.com>
2022-06-13 08:53:32 +0800

    1.mybatis 逆向生成器, 奢华尊享版的基本查询,example 示例。

---
+++ mybatis_MBG/src/test/java/com/johnny/mybatis/mapper/EmpMapperTest.java
@@ -0,0 +1,45 @@
+package com.johnny.mybatis.mapper;
+
+import com.johnny.mybatis.pojo.Emp;
+import com.johnny.mybatis.pojo.EmpExample;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author johnny
+ * @Classname EmpMapperTest
+ * @Description
+ * @Date 2022/6/13 08:31
+ */
+public class EmpMapperTest {
+
+    // 进行测试qbc query by  criteria 基本查询
+    @Test
+    public void selectByExample() {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            SqlSession sqlSession = sessionFactory.openSession(true);
+            EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+           /*
+           查询所有
+           List<Emp> emps = mapper.selectByExample(null);*/
+            EmpExample example=new EmpExample();
+            example.createCriteria().andAgeIn(Arrays.asList(18,19,20,28)).andEmpNameLike("%小青%");
+            List<Emp> emps = mapper.selectByExample(example);
+            emps.forEach(emp -> System.out.println(emp));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
